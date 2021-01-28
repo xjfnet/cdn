84ed3f61ac0996daa27990f1b326b0ebe233d5ea
xjfnet <xjfnet@126.com>
2021-01-28 15:46:58 +0800

    update

--- jquery.extensions.js
+++ jquery.extensions.js
@@ -1,11 +1,64 @@
 jQuery.extend({
+    waitUntil: function waitUntil(predicate, timeout, interval) {
+        var timerInterval = interval || 50;
+        var timerTimeout = timeout || 1000 * 60 * 5;
 
+        return new Promise(function promiseCallback(resolve, reject) {
+            var timer;
+            var timeoutTimer;
+            var clearTimers;
+            var doStep;
+
+            clearTimers = function clearWaitTimers() {
+                clearTimeout(timeoutTimer);
+                clearInterval(timer);
+            };
+
+            doStep = function doTimerStep() {
+                var result;
+
+                try {
+                    result = predicate();
+
+                    if (result) {
+                        clearTimers();
+                        resolve(result);
+                    } else {
+                        timer = setTimeout(doStep, timerInterval);
+                    }
+                } catch (e) {
+                    clearTimers();
+                    reject(e);
+                }
+            };
+
+            timer = setTimeout(doStep, timerInterval);
+            timeoutTimer = setTimeout(function onTimeout() {
+                clearTimers();
+                reject(new Error('Timed out after waiting for ' + timerTimeout + 'ms'));
+            }, timerTimeout);
+        });
+    },
 });
 jQuery.fn.extend({
+    waitUntilExists: function (timeout, interval) {
+        var context = this.context;
+        var selector = this.selector;
+        return jQuery.waitUntil(function () {
+            return !!jQuery(selector, context).length;
+        });
+    },
     selectByText: function (text) {
         $(this).find('option').each(function () {
-            console.log('aaa');
             if ($(this).text().includes(text)) {
+                this.selected = true;
+                return false;
+            }
+        });
+    },
+    selectByValue: function (value) {
+        $(this).find('option').each(function () {
+            if ($(this).val().includes(value)) {
                 this.selected = true;
                 return false;
             }
