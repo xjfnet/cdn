d816f4a15e12a288b338f76cdc6622023fb91c6b
xjfnet <xjfnet@126.com>
2021-01-28 14:18:03 +0800

    relayout

--- 青果教务系统辅助脚本.user.js
+++ 青果教务系统辅助脚本.user.js
@@ -4,10 +4,8 @@
 // @downloadUrl https://cdn.jsdelivr.net/gh/xjfnet/UserScript/青果教务系统辅助脚本.user.js
 // @match       http*://58.51.106.66:9301/*
 // @match       http*://172.18.254.16/*
-// @resource    easyui.css https://gitee.com/QQ56188788/user-script/raw/master/assets/easyui/themes/default/easyui.css
-// @resource    icon.css https://gitee.com/QQ56188788/user-script/raw/master/assets/easyui/themes/icon.css
-// @require     https://gitee.com/QQ56188788/user-script/raw/master/assets/jquery/jquery.min.js
-// @require     https://gitee.com/QQ56188788/user-script/raw/master/assets/easyui/jquery.easyui.min.js
+// @require     https://cdn.jsdelivr.net/gh/xjfnet/UserScript/assets/easyui/jquery.min.js
+// @require     https://cdn.jsdelivr.net/gh/xjfnet/UserScript/assets/easyui/jquery.easyui.min.js
 // @require     https://gitee.com/QQ56188788/user-script/raw/master/assets/js.cookie.js
 // @require     https://gitee.com/QQ56188788/user-script/raw/master/assets/xlsx/xlsx.full.min.js
 // @require     https://gitee.com/QQ56188788/user-script/raw/master/assets/tippy/popper.min.js
@@ -17,3 +15,9 @@
 // @author      夏剑峰
 // @description 青果教务系统辅助功能：成绩模板导出，成绩导入，按姓名导入综合成功
 // ==/UserScript==
+
+(async function($) {
+    "use strict"
+
+
+})(jQuery)
